e48afbefc244c23ba5d488dc5c50be19ed890144
hanam197 <hanam15c4@gmail.com>
2024-04-02 15:39:41 +0700

    add base of App

--- src/eshop-admin/src/App.js
+++ src/eshop-admin/src/App.js
@@ -1,24 +1,148 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  DashboardOutlined,
+  AreaChartOutlined,
+  CameraOutlined,
+  EditOutlined,
+  CommentOutlined,
+  PieChartOutlined,
+  BarChartOutlined,
+  LineChartOutlined,
+
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, Button, theme } from 'antd';
+import { useState } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+
+const { Header, Sider, Content } = Layout;
 
 function App() {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Sider theme="light" trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" >
+          <img src="https://antd-admin.zuiidea.com/logo.svg" alt="logo" width="30" height="30" />
+          <h1> AntD Admin</h1>
+        </div>
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <DashboardOutlined />,
+              label: (
+                <Link to="products" relative="path">
+                  Create Products
+                </Link>
+              ),
+            },
+            {
+              key: '2',
+              icon: <UserOutlined />,
+              label: 'Users',
+            },
+            {
+              key: '3',
+              icon: <CommentOutlined />,
+              label: 'Posts',
+            },
+            {
+              key: '4',
+              icon: <UploadOutlined />,
+              label: 'Request',
+            },
+            {
+              key: '5',
+              icon: <CameraOutlined />,
+              label: 'UI Element',
+              children: [{
+                key: "6",
+                icon: <EditOutlined />,
+                label: 'Editor',
+              },
+              ],
+            },
+            {
+              key: '7',
+              icon: <AreaChartOutlined />,
+              label: 'Charts',
+              children: [{
+                key: "8",
+                icon: <LineChartOutlined />,
+                label: 'LineCharts',
+              },
+              {
+                key: "9",
+                icon: <BarChartOutlined />,
+                label: 'BarCharts',
+              },
+              {
+                key: "10",
+                icon: <PieChartOutlined />,
+                label: 'PieCharts',
+              },]
+            }
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+
+        </Header>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb
+            items={[
+              {
+                title: 'Products',
+              },
+              {
+                title: <a href="#">Users</a>,
+              },
+              {
+                title: <a href="#">Posts</a>,
+              },
+              {
+                title: 'Request',
+              },
+            ]}
+          />
+          <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <Outlet />
+          </Content>
+        </Layout>
+
+      </Layout>
+    </Layout>
   );
 }
 
---
+++ src/eshop-admin/src/component/Products.js
@@ -0,0 +1,6 @@
+function Products() {
+    return (
+        <div>Product</div>
+    )
+}
+export default Products;
